24852ff8354531f25ad14ee5b5a5dac1bb228110
Makgsoewar <makgsoewar5@gmail.com>
2025-04-01 10:07:27 +0630

    change data type int to string for amount

--- src/KbzPayQr.php
+++ src/KbzPayQr.php
@@ -9,7 +9,7 @@ class KbzPayQr extends KbzPay
     /**
      * @throws Exception
      */
-    public function getPaymentQr(string $orderId, int $amount, string $nonceStr, string $backendResultUrl): string
+    public function getPaymentQr(string $orderId, string $amount, string $nonceStr, string $backendResultUrl): string
     {
         return $this->preCreate("PAY_BY_QRCODE", $orderId, $amount, $nonceStr, $backendResultUrl);
     }
